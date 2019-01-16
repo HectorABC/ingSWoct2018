c2ba58ebb84d69befa1d5d9c47880f64176f483e
HectorABC <hectoralarconguerra@hotmail.com>
2019-01-16 11:19:27 -0500

    Se adicionó un comentario sobre el funcionamiento del programa antes de las declaraciones de clases

--- calcularEdad.java
+++ calcularEdad.java
@@ -12,6 +12,14 @@ import java.util.GregorianCalendar;
  *
  * @author Alberto Alarcon
  */
+/*
+FUNCIONAMIENTO DEL PROGRAMA.
+1.	Inicialmente realizamos un import de las librerías necesarias para los cálculos que se explicaran más adelante. 
+2.	Se declara una clase calcularEdad.
+3.	Se declara la función main que muestra los años.
+4.	A continuación se declara la función calcular que recibe un tipo de dato Calendar fechaNac de las librerías que se importó al inicio del programa, se declara fechaActual del tipo de dato Calendar que recibe como dato la fecha actual. Seguidamente se declaran los datos años, meses y días de tipo entero que reciben datos de los cálculos entre los objetos fechaActual y fechaNac respectivamente, que es la resta entre la fecha actual y la de nacimiento para lo cual se usa la función get del objeto Calendar que obtiene los datos respectivamente.
+5.	Finalmente se procede a la compilación y corrida del programa.
+*/
 public class calcularEdad {
     /**
      * @param args the command line arguments
@@ -21,7 +29,7 @@ public class calcularEdad {
         		// TODO Auto-generated method stub
 		// Mostramos el resultado de llamar a la funciÃ³n calcular pasando
 		// como parametro la fecha de nacimiento YYYY-MM-DD
-		System.out.println(calcular(new GregorianCalendar(1977,03,14)));
+		System.out.println(calcular(new GregorianCalendar(2007,03,14)));
     }
         public static int calcular(Calendar fechaNac) {
 
